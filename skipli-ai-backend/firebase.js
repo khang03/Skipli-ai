288b2f354afe50d59afdbebed7e1a7317d3f1969
khang03 <nguyenthekhang2210@gmail.com>
2025-06-02 09:06:47 +0700

    Remove serviceAccountKey.json and add to .gitignore

--- skipli-ai-backend/firebase.js
+++ skipli-ai-backend/firebase.js
@@ -1,6 +1,6 @@
 // firebase.js
-const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json'); // đường dẫn tới file bạn vừa tải
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json"); // đường dẫn tới file bạn vừa tải
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
